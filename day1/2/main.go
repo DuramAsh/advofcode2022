a09471c93f05fa88a53fe21f1786ee9ee4e0e42c
Ашим Жаксылык <zhaksylyka@halyklife.nb>
2023-08-28 09:02:12 +0600

    test doskey

--- day1/2/main.go
+++ day1/2/main.go
@@ -12,7 +12,7 @@ func main() {
 	data, _ := os.Open("./data.txt")
 	defer data.Close()
 	sc := bufio.NewScanner(data)
-	elves := make([]int, 1)
+	elves := make([]int,  1)
 	i := 0
 	for sc.Scan() {
 		text := sc.Text()
